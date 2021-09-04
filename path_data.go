bcc6d6fc8bf186669ee7ad4b4178a6df258a58de
Chris Capurso <ccapurso@hashicorp.com>
2021-09-03 23:03:08 -0400

    provide HandlePatchOperation with PatchPreProcessRules

--- path_data.go
+++ path_data.go
@@ -247,14 +247,17 @@ func (b *versionedKVBackend) pathDataPatch() framework.OperationFunc {
 			return nil, err
 		}
 
-		dataRaw := data.Raw["data"].(map[string]interface{})
 		var existingVersionData *map[string]interface{}
 		err = json.Unmarshal(existingVersion.Data, &existingVersionData)
 		if err != nil {
 			return nil, err
 		}
 
-		modifiedData, err := framework.HandlePatchOperation(req, dataRaw, *existingVersionData, nil)
+		preProcessRules := framework.PatchPreProcessRules{
+			DataKey: "data",
+		}
+
+		modifiedData, err := framework.HandlePatchOperation(req, data, *existingVersionData, preProcessRules)
 		if err != nil {
 			return nil, err
 		}
