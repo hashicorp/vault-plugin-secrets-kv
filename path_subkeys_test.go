27d1fcf9b58179c6bd89bb688c1f1cc21681cdb6
Chris Capurso <ccapurso@hashicorp.com>
2022-01-28 17:25:33 -0500

    adding more subkeys tests

--- path_subkeys_test.go
+++ path_subkeys_test.go
@@ -8,7 +8,22 @@ import (
 	"github.com/hashicorp/vault/sdk/logical"
 )
 
-func TestVersionedKV_Subkeys_Read_CurrentVersion(t *testing.T) {
+func TestVersionedKV_Subkeys_NotFound(t *testing.T) {
+	b, storage := getBackend(t)
+
+	req := &logical.Request{
+		Operation: logical.ReadOperation,
+		Path:      "subkeys/foo",
+		Storage:   storage,
+	}
+
+	resp, err := b.HandleRequest(context.Background(), req)
+	if err != nil || resp != nil {
+		t.Fatalf("unexpected ReadOperation response, err: %s, resp %#v", err.Error(), resp)
+	}
+}
+
+func TestVersionedKV_Subkeys_CurrentVersion(t *testing.T) {
 	b, storage := getBackend(t)
 
 	data := map[string]interface{}{
@@ -20,13 +35,13 @@ func TestVersionedKV_Subkeys_Read_CurrentVersion(t *testing.T) {
 						"d": "does-not-matter",
 					},
 				},
-				"b": "does-not-matter",
+				"b": map[string]interface{}{},
 			},
 			"baz": map[string]interface{}{
-				"e": "does-not-matter",
+				"e": 3.14,
 			},
-			"quux": "does-not-matter",
-			"quuz": "does-not-matter",
+			"quux": 123,
+			"quuz": []string{"does-not-matter"},
 		},
 	}
 
@@ -39,7 +54,7 @@ func TestVersionedKV_Subkeys_Read_CurrentVersion(t *testing.T) {
 
 	resp, err := b.HandleRequest(context.Background(), req)
 	if err != nil || (resp != nil && resp.IsError()) {
-		t.Fatalf("CreateOperation request failed, err: %s, resp %#v", err, resp)
+		t.Fatalf("CreateOperation request failed, err: %s, resp %#v", err.Error(), resp)
 	}
 
 	req = &logical.Request{
@@ -50,24 +65,7 @@ func TestVersionedKV_Subkeys_Read_CurrentVersion(t *testing.T) {
 
 	resp, err = b.HandleRequest(context.Background(), req)
 	if err != nil || (resp != nil && resp.IsError()) {
-		t.Fatalf("ReadOperation request failed, err: %s, resp %#v", err, resp)
-	}
-
-	expectedData := map[string]interface{}{
-		"foo": nil,
-		"bar": map[string]interface{}{
-			"a": map[string]interface{}{
-				"c": map[string]interface{}{
-					"d": nil,
-				},
-			},
-			"b": nil,
-		},
-		"baz": map[string]interface{}{
-			"e": nil,
-		},
-		"quux": nil,
-		"quuz": nil,
+		t.Fatalf("ReadOperation request failed, err: %s, resp %#v", err.Error(), resp)
 	}
 
 	expectedRespKeys := map[string]struct{}{
@@ -88,7 +86,116 @@ func TestVersionedKV_Subkeys_Read_CurrentVersion(t *testing.T) {
 		t.Fatalf("metadata map keys mismatch, diff: %#v", diff)
 	}
 
-	if diff := deep.Equal(resp.Data["subkeys"], expectedData); len(diff) > 0 {
+	expectedSubkeys := map[string]interface{}{
+		"foo": nil,
+		"bar": map[string]interface{}{
+			"a": map[string]interface{}{
+				"c": map[string]interface{}{
+					"d": nil,
+				},
+			},
+			"b": nil,
+		},
+		"baz": map[string]interface{}{
+			"e": nil,
+		},
+		"quux": nil,
+		"quuz": nil,
+	}
+
+	if diff := deep.Equal(resp.Data["subkeys"], expectedSubkeys); len(diff) > 0 {
 		t.Fatalf("resp and expected data mismatch, diff: %#v", diff)
 	}
 }
+
+func TestVersionedKV_Subkeys_EmptyData(t *testing.T) {
+	b, storage := getBackend(t)
+
+	req := &logical.Request{
+		Operation: logical.CreateOperation,
+		Path:      "data/foo",
+		Storage:   storage,
+		Data:      map[string]interface{}{
+			"data": map[string]interface{}{},
+		},
+	}
+
+	resp, err := b.HandleRequest(context.Background(), req)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("CreateOperation request failed, err: %s, resp %#v", err.Error(), resp)
+	}
+
+	req = &logical.Request{
+		Operation: logical.ReadOperation,
+		Path:      "subkeys/foo",
+		Storage:   storage,
+	}
+
+	resp, err = b.HandleRequest(context.Background(), req)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("ReadOperation request failed, err: %s, resp %#v", err.Error(), resp)
+	}
+
+	if diff := deep.Equal(resp.Data["subkeys"], map[string]interface{}{}); len(diff) > 0 {
+		t.Fatalf("resp and expected data mismatch, diff: %#v", diff)
+	}
+}
+
+func TestVersionedKV_Subkeys_MaxDepth(t *testing.T) {
+	b, storage := getBackend(t)
+
+	maxSubkeysDepth = 3
+
+	req := &logical.Request{
+		Operation: logical.CreateOperation,
+		Path:      "data/foo",
+		Storage:   storage,
+		Data:      map[string]interface{}{
+			"data": map[string]interface{}{
+				"foo": map[string]interface{}{
+					"bar": map[string]interface{}{
+						"baz": 123,
+					},
+				},
+			},
+		},
+	}
+
+	resp, err := b.HandleRequest(context.Background(), req)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("CreateOperation request failed, err: %s, resp %#v", err.Error(), resp)
+	}
+
+	req = &logical.Request{
+		Operation: logical.ReadOperation,
+		Path:      "subkeys/foo",
+		Storage:   storage,
+	}
+
+	resp, err = b.HandleRequest(context.Background(), req)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("ReadOperation request failed, err: %s, resp %#v", err.Error(), resp)
+	}
+
+	expectedSubKeys := map[string]interface{}{
+		"foo": map[string]interface{}{
+			"bar": map[string]interface{}{
+				"baz": nil,
+			},
+		},
+	}
+
+	if diff := deep.Equal(resp.Data["subkeys"], expectedSubKeys); len(diff) > 0 {
+		t.Fatalf("resp and expected data mismatch, diff: %#v", diff)
+	}
+}
+
+func TestVersionedKV_Subkeys_VersionDeleted(t *testing.T) {
+	// TODO
+}
+
+func TestVersionedKV_Subkeys_VersionDestroyed(t *testing.T) {
+	// TODO
+}
+
+
