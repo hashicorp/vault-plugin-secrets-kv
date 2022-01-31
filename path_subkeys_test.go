0ec2e882505b98cd2ca99f07e0232d82d564d131
Chris Capurso <ccapurso@hashicorp.com>
2022-01-31 10:46:16 -0500

    adding more unit tests

--- path_subkeys_test.go
+++ path_subkeys_test.go
@@ -2,6 +2,8 @@ package kv
 
 import (
 	"context"
+	"encoding/json"
+	"net/http"
 	"testing"
 
 	"github.com/go-test/deep"
@@ -108,6 +110,64 @@ func TestVersionedKV_Subkeys_CurrentVersion(t *testing.T) {
 	}
 }
 
+func TestVersionedKV_Subkeys_VersionParam(t *testing.T) {
+	b, storage := getBackend(t)
+
+	req := &logical.Request{
+		Operation: logical.CreateOperation,
+		Path:      "data/foo",
+		Storage:   storage,
+		Data: map[string]interface{}{
+			"data": map[string]interface{}{
+				"foo": "abc",
+			},
+		},
+	}
+
+	resp, err := b.HandleRequest(context.Background(), req)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("data CreateOperation request failed, err: %s, resp %#v", err.Error(), resp)
+	}
+
+	req = &logical.Request{
+		Operation: logical.CreateOperation,
+		Path:      "data/foo",
+		Storage:   storage,
+		Data: map[string]interface{}{
+			"data": map[string]interface{}{
+				"foo": "abc",
+				"bar": "def",
+			},
+		},
+	}
+
+	resp, err = b.HandleRequest(context.Background(), req)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("data CreateOperation request failed, err: %s, resp %#v", err.Error(), resp)
+	}
+
+	req = &logical.Request{
+		Operation: logical.ReadOperation,
+		Path:      "subkeys/foo",
+		Storage:   storage,
+		Data:      map[string]interface{}{
+			"version": 1,
+		},
+	}
+
+	resp, err = b.HandleRequest(context.Background(), req)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("subkeys ReadOperation request failed, err: %s, resp %#v", err.Error(), resp)
+	}
+
+	expectedSubkeys := map[string]interface{}{
+		"foo": nil,
+	}
+	if diff := deep.Equal(resp.Data["subkeys"], expectedSubkeys); len(diff) > 0 {
+		t.Fatalf("resp and expected data mismatch, diff: %#v", diff)
+	}
+}
+
 func TestVersionedKV_Subkeys_EmptyData(t *testing.T) {
 	b, storage := getBackend(t)
 
@@ -191,9 +251,184 @@ func TestVersionedKV_Subkeys_MaxDepth(t *testing.T) {
 }
 
 func TestVersionedKV_Subkeys_VersionDeleted(t *testing.T) {
-	// TODO
+	b, storage := getBackend(t)
+
+	req := &logical.Request{
+		Operation: logical.CreateOperation,
+		Path:      "data/foo",
+		Storage:   storage,
+		Data: map[string]interface{}{
+			"data": map[string]interface{}{
+				"foo": "bar",
+			},
+		},
+	}
+
+	resp, err := b.HandleRequest(context.Background(), req)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("data CreateOperation request failed, err: %s, resp %#v", err.Error(), resp)
+	}
+
+	req = &logical.Request{
+		Operation: logical.ReadOperation,
+		Path:      "subkeys/foo",
+		Storage:   storage,
+	}
+
+	resp, err = b.HandleRequest(context.Background(), req)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("subkeys ReadOperation request failed, err: %s, resp %#v", err.Error(), resp)
+	}
+
+	expectedSubkeys := map[string]interface{}{
+		"foo": nil,
+	}
+	if diff := deep.Equal(resp.Data["subkeys"], expectedSubkeys); len(diff) > 0 {
+		t.Fatalf("resp and expected data mismatch, diff: %#v", diff)
+	}
+
+	req = &logical.Request{
+		Operation: logical.DeleteOperation,
+		Path:      "data/foo",
+		Storage:   storage,
+	}
+
+	resp, err = b.HandleRequest(context.Background(), req)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("metadata DeleteOperation request failed - err: %s resp: %#v\n", err.Error(), resp)
+	}
+
+	req = &logical.Request{
+		Operation: logical.ReadOperation,
+		Path:      "subkeys/foo",
+		Storage:   storage,
+	}
+
+	resp, err = b.HandleRequest(context.Background(), req)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("subkeys ReadOperation request failed, err: %s, resp %#v", err.Error(), resp)
+	}
+
+	// Use of logical.RespondWithStatusCode in handler will
+	// serialize the JSON response body as a string
+	respBody := map[string]interface{}{}
+
+	if resp.Data["http_status_code"] != http.StatusNotFound {
+		t.Fatalf("expected 404 response for subkeys ReadOperation: resp: %#v", resp)
+	}
+
+	if rawRespBody, ok := resp.Data[logical.HTTPRawBody]; ok {
+		err = json.Unmarshal([]byte(rawRespBody.(string)), &respBody)
+	}
+
+	respDataRaw, ok := respBody["data"]
+	if !ok {
+		t.Fatalf("no data provided in subkeys response, resp: %#v\n", resp)
+	}
+
+	respData := respDataRaw.(map[string]interface{})
+
+	respMetadataRaw, ok := respData["metadata"]
+	if !ok {
+		t.Fatalf("no metadata provided in subkeys response, resp: %#v\n", resp)
+	}
+
+	respMetadata := respMetadataRaw.(map[string]interface{})
+
+	if respMetadata["deletion_time"] == "" {
+		t.Fatalf("expected deletion_time to be set, resp: %#v\n", resp)
+	}
 }
 
 func TestVersionedKV_Subkeys_VersionDestroyed(t *testing.T) {
-	// TODO
+	b, storage := getBackend(t)
+
+	req := &logical.Request{
+		Operation: logical.CreateOperation,
+		Path:      "data/foo",
+		Storage:   storage,
+		Data: map[string]interface{}{
+			"data": map[string]interface{}{
+				"foo": "bar",
+			},
+		},
+	}
+
+	resp, err := b.HandleRequest(context.Background(), req)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("data CreateOperation request failed, err: %s, resp %#v", err.Error(), resp)
+	}
+
+	req = &logical.Request{
+		Operation: logical.ReadOperation,
+		Path:      "subkeys/foo",
+		Storage:   storage,
+	}
+
+	resp, err = b.HandleRequest(context.Background(), req)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("subkeys ReadOperation request failed, err: %s, resp %#v", err.Error(), resp)
+	}
+
+	expectedSubkeys := map[string]interface{}{
+		"foo": nil,
+	}
+	if diff := deep.Equal(resp.Data["subkeys"], expectedSubkeys); len(diff) > 0 {
+		t.Fatalf("resp and expected data mismatch, diff: %#v", diff)
+	}
+
+	req = &logical.Request{
+		Operation: logical.CreateOperation,
+		Path:      "destroy/foo",
+		Storage:   storage,
+		Data:      map[string]interface{}{
+			"versions": []int{1},
+		},
+	}
+
+	resp, err = b.HandleRequest(context.Background(), req)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("destroy CreateOperation request failed - err: %s resp:%#v\n", err.Error(), resp)
+	}
+
+	req = &logical.Request{
+		Operation: logical.ReadOperation,
+		Path:      "subkeys/foo",
+		Storage:   storage,
+	}
+
+	resp, err = b.HandleRequest(context.Background(), req)
+	if err != nil || (resp != nil && resp.IsError()) {
+		t.Fatalf("subkeys ReadOperation request failed, err: %s, resp %#v", err.Error(), resp)
+	}
+
+	// Use of logical.RespondWithStatusCode in handler will
+	// serialize the JSON response body as a string
+	respBody := map[string]interface{}{}
+
+	if resp.Data["http_status_code"] != http.StatusNotFound {
+		t.Fatalf("expected 404 response for subkeys ReadOperation: resp:%#v", resp)
+	}
+
+	if rawRespBody, ok := resp.Data[logical.HTTPRawBody]; ok {
+		err = json.Unmarshal([]byte(rawRespBody.(string)), &respBody)
+	}
+
+	respDataRaw, ok := respBody["data"]
+	if !ok {
+		t.Fatalf("no data provided in subkeys response, resp: %#v\n", resp)
+	}
+
+	respData := respDataRaw.(map[string]interface{})
+
+	respMetadataRaw, ok := respData["metadata"]
+	if !ok {
+		t.Fatalf("no metadata provided in subkeys response, resp: %#v\n", resp)
+	}
+
+	respMetadata := respMetadataRaw.(map[string]interface{})
+
+	if respMetadata["destroyed"] == nil || !respMetadata["destroyed"].(bool) {
+		t.Fatalf("expected version to be destroyed, resp: %#v\n", resp)
+	}
 }
